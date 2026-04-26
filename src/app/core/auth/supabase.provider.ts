b250e8654f506a0ae0a515d629caa0be0e84a4b6
DarenTx <daren@dahlkids.com>
2026-04-26 13:53:30 -0500

    Fixed authn issue

--- src/app/core/auth/supabase.provider.ts
+++ src/app/core/auth/supabase.provider.ts
@@ -13,6 +13,7 @@ export function provideSupabase() {
           auth: {
             flowType: 'pkce',
             storage: localStorage,
+            detectSessionInUrl: false,
           },
         }),
     },
